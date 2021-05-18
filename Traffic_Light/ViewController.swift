36efc4fffc74e694afb3ab9042c08aeda481b890
Alexander Kovzhut <akovzhut@icloud.com>
2021-05-18 23:17:36 +0300

    Initial commit

--- Traffic_Light/ViewController.swift
+++ Traffic_Light/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet weak var greenColorView: UIView!
+    @IBOutlet weak var nextPressedButton: UIButton!
+    
+    override func viewDidLayoutSubviews() {
+        
+        super.viewDidLayoutSubviews()
+        
+        redColorView.alpha = 0.3
+        yellowColorView.alpha = 0.3
+        greenColorView.alpha = 0.3
+        
+      }
+    
+    
+    
+    @IBAction func startPressedButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            
+            redColorView.alpha = 1
+            yellowColorView.alpha = 0.3
+            greenColorView.alpha = 0.3
+            
+            nextPressedButton.setTitle("NEXT", for: .normal)
+        } else {
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 1
+            greenColorView.alpha = 0.3
+            
+        }
+        }
+        
+    
+    
+    
+    
+    
+    
+    @IBAction func stopPressedButton(_ sender: UIButton) {
+        
+        redColorView.alpha = 0.3
+        yellowColorView.alpha = 0.3
+        greenColorView.alpha = 0.3
+        
+        nextPressedButton.setTitle("START", for: .normal)
     }
-
-
 }
 
