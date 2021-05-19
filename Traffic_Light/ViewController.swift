2f6d01f470788307dd2dfcfaef345a19ab1704c0
Alexander Kovzhut <akovzhut@icloud.com>
2021-05-19 20:49:11 +0300

    Initial commit

--- Traffic_Light/ViewController.swift
+++ Traffic_Light/ViewController.swift
@@ -9,54 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var redColorView: UIView!
-    @IBOutlet weak var yellowColorView: UIView!
-    @IBOutlet weak var greenColorView: UIView!
-    @IBOutlet weak var nextPressedButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var redUIView: UIView!
+    @IBOutlet weak var yellowUIView: UIView!
+    @IBOutlet weak var greenUIView: UIView!
     
-    override func viewDidLayoutSubviews() {
-        
-        super.viewDidLayoutSubviews()
-        
-        redColorView.alpha = 0.3
-        yellowColorView.alpha = 0.3
-        greenColorView.alpha = 0.3
-        
-      }
-    
-    
-    
-    @IBAction func startPressedButton(_ sender: UIButton) {
-        sender.isSelected = !sender.isSelected
-        
-        if sender.isSelected {
-            
-            redColorView.alpha = 1
-            yellowColorView.alpha = 0.3
-            greenColorView.alpha = 0.3
-            
-            nextPressedButton.setTitle("NEXT", for: .normal)
-        } else {
-            redColorView.alpha = 0.3
-            yellowColorView.alpha = 1
-            greenColorView.alpha = 0.3
-            
-        }
-        }
-        
-    
-    
-    
-    
-    
-    
-    @IBAction func stopPressedButton(_ sender: UIButton) {
-        
-        redColorView.alpha = 0.3
-        yellowColorView.alpha = 0.3
-        greenColorView.alpha = 0.3
-        
-        nextPressedButton.setTitle("START", for: .normal)
-    }
-}
+    var isFadeOn = true
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redUIView.alpha = 0.3
+        yellowUIView.alpha = 0.3
+        greenUIView.alpha = 0.3
+        
+    }
+    
+    
+  
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+       
+        
+    }
+
+}
